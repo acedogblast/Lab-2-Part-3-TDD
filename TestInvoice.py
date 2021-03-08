607ae23c0dede6cd3600a9bb8b358bbaae9eecb9
acedogblast <charles2000wang@gmail.com>
2021-03-08 16:42:15 -0500

    Part 4

--- TestInvoice.py
+++ TestInvoice.py
@@ -24,3 +24,15 @@ def test_CanCalculateTotalDiscount(invoice, products):
 def test_CanCalculateTotalPurePrice(invoice, products):
     invoice.totalImpurePrice(products)
     assert invoice.totalPurePrice(products) == 69.38
+
+def test_CanCalculateOrderSim1(invoice):
+    invoice = Invoice()
+    products = {'Computer': {'qnt': 10, 'unit_price': 999.99, 'discount': 3.1415},
+                'Laptop': {'qnt': 5, 'unit_price': 799.99, 'discount': 0}}
+    assert invoice.totalPurePrice(products) == 13685.7
+
+def test_CanCalculateOrderSim2(invoice):
+    invoice = Invoice()
+    products = {'Pen': {'qnt': 0, 'unit_price': 3.75, 'discount': 5},
+                'Notebook': {'qnt': 1, 'unit_price': 7.5, 'discount': 100}}
+    assert invoice.totalPurePrice(products) == 0.0
